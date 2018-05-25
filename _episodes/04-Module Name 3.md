b522bb99967cba6d9f307f5bdd5f5cf6a2578bde
manishavyas <32226760+manishavyas@users.noreply.github.com>
2018-05-25 14:04:07 -0700

    mozilla code added as base

--- _episodes/04-Module Name 3.md
+++
@@ -1,7 +0,0 @@
----
-layout: break
-title: "Module 3"
-teaching: 0
-exercises: 0
-break: 15
----
---
+++ web-lit-basics-one/activity-menu.js
@@ -0,0 +1,40 @@
+var title = "Web Literacy Basics I";
+
+var activities = [
+{
+title : "Kraken the Code",
+url: "session01-kraken-the-code.html"
+},
+{
+title : "Ping Kong",
+url: "session02-ping-kong.html"
+},
+{
+title : "Hack the News",
+url: "session03-hack-the-news.html"
+},
+{
+title : "HTML Puzzle Boxes",
+url: "session04-html-puzzle-boxes.html"
+},
+{
+title : "Web Chef",
+url: "session05-web-chef.html"
+},
+{
+title : "A Story of Self",
+url: "session06-story-of-self.html"
+},
+{
+title : "Create Secure Passwords",
+url: "session07-create-secure-passwords.html"
+},
+{
+title : "Cookies and Third-Party Tracking",
+url: "session08-cookies-and-third-party-tracking.html"
+},
+{
+title : "Data Trail Timeline",
+url: "session09-data-trail-timeline.html"
+}
+];
